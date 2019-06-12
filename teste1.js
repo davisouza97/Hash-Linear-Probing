d78c69d3d3c88f631f8d6d7e04c23c82bb0cba1a
davisouza97 <davisouza1997jf@gmail.com>
2019-06-12 09:44:16 -0300

    fudeeeeeeeeu

--- teste1.js
+++ teste1.js
@@ -3,40 +3,72 @@ class Hash {
         this.lista = new Array(numero); //this == publico/ var == private
         this.size = this.lista.length;
 
-        this.add = function (indice, valor) {
-            if (indice < 0 || indice >= this.size) {
+        this.mod = function(indice){
+            return indice % this.size;
+        };
+
+        this.add = function (item) {
+            console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@   "+item.chave);
+
+            var mod = this.mod(item.chave);
+
+            console.log("mod add ::  "+mod);
+
+            if (mod < 0 || mod >= this.size) {
                 return;
             }
-            if (this.lista[indice] === undefined) //se vazio
+            if (this.lista[mod] === undefined) //se vazio
             {
-                this.lista[indice] = valor;
+                this.lista[mod] = item;
             } else {
-                this.colisao(indice, valor);
+                this.colisao(item);
             }
         };
-        this.colisao = function (indice, valor) {  //adicionar um controle para evitar loop infinito se a lista estiver cheia 
-            for (let i = indice; i < this.size;) {
+        this.colisao = function (item) {  //adicionar um controle para evitar loop infinito se a lista estiver cheia 
+            for (let i = this.mod(item.chave); i < this.size;) {
                 if (this.lista[i] === undefined) {
-                    this.lista[i] = valor;
+                    this.lista[i] = item;
                     break;
                 }
-                if (i === this.size-1) {
+                if (i === this.size - 1) {
                     i = -1;
                 }
                 i++;
             }
         };
+        this.excluir = function (item) {
+            var mod = this.mod(item.chave);
+            console.log("mod remoção ::  "+mod);
+            if (mod < 0 || mod >= this.size || this.lista[mod] === undefined) {
+                console.log("valor não existe")
+                return;
+            }else{
+            }
+        }
     };
 }
 
-var hash = new Hash(10);
+class Item {
+    constructor(chave) {
+        this.chave = chave;
+        this.dado = "#" + chave + "#";
+    }
+}
 
-hash.add(9, 40);
-hash.add(9, 50);
-hash.add(9, 100);
+var hash = new Hash(13);
+
+var um = new Item(0);
+var dois = new Item(1);
+var tres = new Item(13);
+
+hash.add(um);
+hash.add(dois);
+hash.add(tres);
 
 console.log("############################");
 
 for (let index = 0; index < hash.lista.length; index++) {
     console.log(hash.lista[index]);
 }
+
+console.log("############################");
