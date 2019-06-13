4d20adcf0244c4ea0f90ace64778d69de0e0625e
davisouza97 <davisouza1997jf@gmail.com>
2019-06-13 11:37:24 -0300

    teste

--- teste1.js
+++ teste1.js
@@ -1,18 +1,22 @@
+var div = document.getElementById("lista");
+var botao = document.getElementById("botao");
+
+
 class Hash {
     constructor(numero) {
         this.lista = new Array(numero); //this == publico/ var == private
         this.size = this.lista.length;
-
-        this.mod = function(indice){
+        var controleLoop = 0;
+        this.mod = function (indice) {
             return indice % this.size;
         };
 
         this.add = function (item) {
-            console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@   "+item.chave);
+            console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@   " + item.chave);
 
             var mod = this.mod(item.chave);
 
-            console.log("mod add ::  "+mod);
+            console.log("mod add ::  " + mod);
 
             if (mod < 0 || mod >= this.size) {
                 return;
@@ -25,7 +29,8 @@ class Hash {
             }
         };
         this.colisao = function (item) {  //adicionar um controle para evitar loop infinito se a lista estiver cheia 
-            for (let i = this.mod(item.chave); i < this.size;) {
+            var modulo = this.mod(item.chave);
+            for (let i = modulo; i < this.size;) {
                 if (this.lista[i] === undefined) {
                     this.lista[i] = item;
                     break;
@@ -34,19 +39,62 @@ class Hash {
                     i = -1;
                 }
                 i++;
+                if (i === modulo) {
+                    console.log("lista percorrida inteiramente");
+                    break;
+                }
             }
         };
         this.excluir = function (item) {
-            var mod = this.mod(item.chave);
-            console.log("mod remoção ::  "+mod);
-            if (mod < 0 || mod >= this.size || this.lista[mod] === undefined) {
+            var modulo = this.mod(item.chave);
+            console.log("mod remoção ::  " + modulo);
+            if (modulo < 0 || modulo >= this.size || this.lista[modulo] === undefined) {
                 console.log("valor não existe")
                 return;
-            }else{
+            } else {
+                for (let i = this.mod(item.chave); i < this.size;) {
+                    if (this.lista[i].chave === item.chave) {
+                        console.log("Remocao" + this.lista[i].chave + "--" + this.lista[i].dado);
+                        this.lista[i] = undefined;
+                        if (this.lista[i + 1] !== undefined) {
+                            this.reposicionar(i, i + 1);
+                        }
+                        break;
+                    }
+                    if (i === this.size - 1) {
+                        i = -1;
+                    }
+                    i++;
+                    if (i === modulo) {
+                        console.log("lista percorrida inteiramente");
+                        break;
+                    }
+                }
             }
         }
-    };
-}
+        this.reposicionar = function (destino, origem) {
+            var posicao = origem;
+            if (this.lista[posicao] === undefined) {
+                return;
+            }
+            else {
+                if (this.mod(this.lista[posicao].chave) !== posicao) {  //colisao
+                    this.lista[destino] = this.lista[posicao];
+                    this.lista[posicao] = undefined;
+                    return
+                }
+                if (this.mod(this.lista[posicao].chave) === posicao) { //nao colisao
+                    this.reposicionar(destino, posicao + 1);
+                }
+            }
+            if (this.lista[posicao + 1] !== undefined) {
+                this.reposicionar(posicao, posicao + 1);
+            }
+
+        }
+    }
+
+};
 
 class Item {
     constructor(chave) {
@@ -55,15 +103,16 @@ class Item {
     }
 }
 
+/*
 var hash = new Hash(13);
 
 var um = new Item(0);
 var dois = new Item(1);
 var tres = new Item(13);
 
-hash.add(um);
-hash.add(dois);
-hash.add(tres);
+hash.add(um);   //pos 0
+hash.add(dois); //pos 1
+hash.add(tres); //pos 0(vai para 2)
 
 console.log("############################");
 
@@ -72,3 +121,15 @@ for (let index = 0; index < hash.lista.length; index++) {
 }
 
 console.log("############################");
+
+hash.excluir(dois);
+
+console.log("############################");
+
+for (let index = 0; index < hash.lista.length; index++) {
+    console.log(hash.lista[index]);
+}
+
+console.log("############################");
+
+*/
