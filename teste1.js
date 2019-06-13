3410e894106835d14d779d95491a4c3505d5dca9
davisouza97 <davisouza1997jf@gmail.com>
2019-06-13 15:07:45 -0300

    socorro

--- teste1.js
+++ teste1.js
@@ -1,38 +1,60 @@
-var div = document.getElementById("lista");
-var botao = document.getElementById("botao");
+function Hash(numero) {
+    this.lista = new Array(parseInt(numero)); //this == publico/ var == private
+    this.size = this.lista.length;
+    this.mod = function (indice) {
+        return indice % this.size;
+    };
 
+    this.add = function (item) {
+        var mod = this.mod(item.chave);
+        //console.log("mod add ::  " + mod);
 
-class Hash {
-    constructor(numero) {
-        this.lista = new Array(numero); //this == publico/ var == private
-        this.size = this.lista.length;
-        var controleLoop = 0;
-        this.mod = function (indice) {
-            return indice % this.size;
-        };
+        if (mod < 0 || mod >= this.size) {
+            return;
+        }
+        if (this.lista[mod] === undefined) //se vazio
+        {
+            this.lista[mod] = item;
+        } else {
 
-        this.add = function (item) {
-            console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@   " + item.chave);
-
-            var mod = this.mod(item.chave);
-
-            console.log("mod add ::  " + mod);
-
-            if (mod < 0 || mod >= this.size) {
+            this.colisao(item);
+        }
+    };
+    this.colisao = function (item) {  //adicionar um controle para evitar loop infinito se a lista estiver cheia 
+        var modulo = this.mod(item.chave);
+        for (let i = modulo; i < this.size;) {
+            if (this.lista[i] != undefined &&this.lista[i].dado === item.dado) {
+                console.log("item ja add");
                 return;
             }
-            if (this.lista[mod] === undefined) //se vazio
-            {
-                this.lista[mod] = item;
-            } else {
-                this.colisao(item);
+            if (this.lista[i] === undefined) {
+                this.lista[i] = item;
+                break;
             }
-        };
-        this.colisao = function (item) {  //adicionar um controle para evitar loop infinito se a lista estiver cheia 
-            var modulo = this.mod(item.chave);
-            for (let i = modulo; i < this.size;) {
-                if (this.lista[i] === undefined) {
-                    this.lista[i] = item;
+            if (i === this.size - 1) {
+                i = -1;
+            }
+            i++;
+            if (i === modulo) {
+                console.log("lista percorrida inteiramente");
+                break;
+            }
+        }
+    };
+    this.excluir = function (chave) {
+        var modulo = this.mod(chave);
+        //console.log("mod remoção ::  " + modulo);
+        if (modulo < 0 || modulo >= this.size || this.lista[modulo] === undefined) {
+            console.log("valor não existe")
+            return;
+        } else {
+            for (let i = this.mod(chave); i < this.size;) {
+                if (this.lista[i].chave === chave) {
+                    console.log("Remocao" + this.lista[i].chave + "--" + this.lista[i].dado);
+                    this.lista[i] = undefined;
+                    if (this.lista[i + 1] !== undefined) {
+                        this.reposicionar(i, i + 1);
+                    }
                     break;
                 }
                 if (i === this.size - 1) {
@@ -44,92 +66,88 @@ class Hash {
                     break;
                 }
             }
-        };
-        this.excluir = function (item) {
-            var modulo = this.mod(item.chave);
-            console.log("mod remoção ::  " + modulo);
-            if (modulo < 0 || modulo >= this.size || this.lista[modulo] === undefined) {
-                console.log("valor não existe")
-                return;
-            } else {
-                for (let i = this.mod(item.chave); i < this.size;) {
-                    if (this.lista[i].chave === item.chave) {
-                        console.log("Remocao" + this.lista[i].chave + "--" + this.lista[i].dado);
-                        this.lista[i] = undefined;
-                        if (this.lista[i + 1] !== undefined) {
-                            this.reposicionar(i, i + 1);
-                        }
-                        break;
-                    }
-                    if (i === this.size - 1) {
-                        i = -1;
-                    }
-                    i++;
-                    if (i === modulo) {
-                        console.log("lista percorrida inteiramente");
-                        break;
-                    }
-                }
-            }
-        }
-        this.reposicionar = function (destino, origem) {
-            var posicao = origem;
-            if (this.lista[posicao] === undefined) {
-                return;
-            }
-            else {
-                if (this.mod(this.lista[posicao].chave) !== posicao) {  //colisao
-                    this.lista[destino] = this.lista[posicao];
-                    this.lista[posicao] = undefined;
-                    return
-                }
-                if (this.mod(this.lista[posicao].chave) === posicao) { //nao colisao
-                    this.reposicionar(destino, posicao + 1);
-                }
-            }
-            if (this.lista[posicao + 1] !== undefined) {
-                this.reposicionar(posicao, posicao + 1);
-            }
-
         }
     }
+    this.reposicionar = function (destino, origem) {
+        var posicao = origem;
+        if (this.lista[posicao] === undefined) {
+            return;
+        }
+        else {
+            if (this.mod(this.lista[posicao].chave) !== posicao) {  //colisao
+                this.lista[destino] = this.lista[posicao];
+                this.lista[posicao] = undefined;
+                return
+            }
+            if (this.mod(this.lista[posicao].chave) === posicao) { //nao colisao
+                this.reposicionar(destino, posicao + 1);
+            }
+        }
+        if (this.lista[posicao + 1] !== undefined) {
+            this.reposicionar(posicao, posicao + 1);
+        }
 
-};
-
-class Item {
-    constructor(chave) {
-        this.chave = chave;
-        this.dado = "#" + chave + "#";
     }
 }
 
-/*
-var hash = new Hash(13);
 
-var um = new Item(0);
-var dois = new Item(1);
-var tres = new Item(13);
-
-hash.add(um);   //pos 0
-hash.add(dois); //pos 1
-hash.add(tres); //pos 0(vai para 2)
-
-console.log("############################");
-
-for (let index = 0; index < hash.lista.length; index++) {
-    console.log(hash.lista[index]);
+function Item(chave, nome) {
+    this.chave = chave;
+    this.dado = nome;
 }
 
-console.log("############################");
-
-hash.excluir(dois);
-
-console.log("############################");
-
-for (let index = 0; index < hash.lista.length; index++) {
-    console.log(hash.lista[index]);
+var listElement = document.getElementById("lista");
+var botao = document.getElementById("botao");
+var input1 = document.getElementById("input1");
+var id = document.getElementById("input2");
+var nome = document.getElementById("input3");
+var hash;
+function criaLista() {
+    if (input1.value != "") {
+        console.log(input1.value);
+        hash = new Hash(input1.value);
+        console.log(hash)
+        //https://pt.stackoverflow.com/questions/4605/remover-elemento-da-p%C3%A1gina-com-javascript    
+        if (botao.parentNode) {
+            botao.parentNode.removeChild(botao);
+            input1.parentElement.removeChild(input1);
+        }
+        renderHash();
+    }
 }
 
-console.log("############################");
+function renderHash() {
+    console.log(hash);
 
-*/
+    var tabela = document.getElementById("lista");
+    var tamanho = tabela.rows.length;
+    for (var index = 0; index < tamanho; index++) {
+        tabela.deleteRow(0);
+    }
+
+    for (item of hash.lista) {
+        var numeroLinhas = tabela.rows.length;
+        var linha = tabela.insertRow(numeroLinhas);
+        var celula1 = linha.insertCell(0);
+        if (item == undefined) {
+            celula1.innerHTML = "vazio";
+        } else {
+            celula1.innerHTML = item.dado;
+        }
+    }
+}
+
+function add() {
+    var campoId = id.value;
+    var campoNome = nome.value;
+    var item = new Item(campoId, campoNome);
+    hash.add(item);
+    renderHash();
+
+}
+
+function remove() {
+    var vlr = id.value;
+    hash.excluir(vlr);
+    renderHash();
+}
