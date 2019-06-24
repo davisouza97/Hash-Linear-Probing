cdecd63b95610b17de658d0a9d2ed3142f384ad2
davisouza97 <davisouza1997jf@gmail.com>
2019-06-23 22:09:47 -0300

    buscar e alert

--- hash.js
+++ hash.js
@@ -2,11 +2,11 @@ function Hash(numero) {
     this.lista = new Array(parseInt(numero)); //this == publico/ var == private
     this.size = this.lista.length;
 
-    this.mod = function (indice) {
+    this.mod = function(indice) {
         return indice % this.size;
     };
 
-    this.add = function (item) {
+    this.add = function(item) {
         var mod = this.mod(item.chave);
         //console.log("mod add ::  " + mod);
         if (mod < 0 || mod >= this.size) {
@@ -21,18 +21,19 @@ function Hash(numero) {
         }
     };
 
-    this.colisao = function (item) {  //adicionar um controle para evitar loop infinito se a lista estiver cheia 
+    this.colisao = function(item) { //adicionar um controle para evitar loop infinito se a lista estiver cheia 
         var modulo = this.mod(item.chave);
         for (let i = modulo; i < this.size;) {
             if (this.lista[i] != undefined && this.lista[i].chave === item.chave) {
-                alert(item.chave + " item já existe existente");
+                if (alerta) alert(item.chave + " item já existe existente");
                 console.log("item ja add");
                 return;
             }
             if (this.lista[i] === undefined) {
-                alert(item.chave + " colidiu com chave existente " + modulo + " e foi encaminhada para o proximo vazio!");
                 this.lista[i] = item;
                 break;
+            } else {
+                if (alerta) alert(item.chave + " colidiu com chave existente " + this.lista[i].chave + " e foi encaminhada para o proximo vazio!");
             }
             if (i === this.size - 1) {
                 i = -1;
@@ -44,16 +45,22 @@ function Hash(numero) {
             }
         }
     };
-    this.excluir = function (chave) {
+    this.excluir = function(chave) {
         var modulo = this.mod(chave);
         //console.log("mod remoção ::  " + modulo);
         if (modulo < 0 || modulo >= this.size || this.lista[modulo] === undefined) {
             console.log("valor não existe");
+            if (alert) alert("O item que você tentou retirar não existe");
             return;
         } else {
             for (let i = this.mod(chave); i < this.size;) {
-                if (this.lista[i].chave === chave) {
+                if (this.lista[i] === undefined) {
+                    console.log("Item com chave: " + chave + " não existe");
+                    if (alert) alert("O item que você tentou retirar não existe");
+                    break;
+                } else if (this.lista[i].chave === chave) {
                     console.log("Remocao" + this.lista[i].chave + "--" + this.lista[i].dado);
+                    if (alerta) alert("Item com chave " + chave + " encontrado e removido")
                     this.lista[i] = undefined;
                     this.reposicionar(i, i + 1);
                     break;
@@ -63,21 +70,20 @@ function Hash(numero) {
                 }
                 i++;
                 if (i === modulo) {
-                    console.log("lista percorrida inteiramente");
+                    if (alerta) alert("lista percorrida inteiramente e item com chave:" + chave + "não foi encontrado");
                     break;
                 }
             }
         }
     }
-    this.reposicionar = function (destino, proximo) {
-        if (proximo >= this.size) {
+    this.reposicionar = function(destino, proximo) {
+        if (proximo >= this.size) { //controle para depois da uçtima posicao voltar pra zero
             proximo = 0;
         }
-        if (this.lista[proximo] === undefined) {//posição seguinte vazia(abortar)
+        if (this.lista[proximo] === undefined) { //posição seguinte vazia(não é preciso reposicionar nada)
             return;
-        }
-        else {
-            if (this.mod(this.lista[proximo].chave) == proximo) {//item já tá no lugar certo
+        } else {
+            if (this.mod(this.lista[proximo].chave) == proximo) { //item já tá no lugar certo
                 // proximo++;
                 this.reposicionar(destino, proximo + 1);
             } else {
@@ -93,6 +99,34 @@ function Hash(numero) {
                     proximo = -1;
                 }
                 this.reposicionar(destino, proximo + 1);
+            }
+        }
+    }
+    this.buscar = function(chave) {
+        var modulo = this.mod(chave);
+        if (modulo < 0 || modulo >= this.size || this.lista[modulo] === undefined) {
+            console.log("valor não existe");
+            if (alerta) alert("O item que você procurou não existe");
+            return;
+        } else {
+            for (let i = this.mod(chave); i < this.size;) {
+                if (this.lista[i] === undefined) {
+                    console.log("Item com chave: " + chave + " não existe");
+                    if (alerta) alert("O item que você procurou não existe");
+                    break;
+                } else if (this.lista[i].chave === chave) {
+                    console.log("Item de chave: " + chave + " encontrada na posição: " + i);
+                    if (alerta) alert("Item de chave: " + chave + " encontrada na posição: " + i);
+                    break;
+                }
+                if (i === this.size - 1) {
+                    i = -1;
+                }
+                i++;
+                if (i === modulo) {
+                    if (alerta) alert("lista percorrida inteiramente e item com chave:" + chave + "não foi encontrado");
+                    break;
+                }
             }
         }
     }
@@ -114,11 +148,14 @@ var input1 = document.getElementById("input1");
 var id = document.getElementById("input2");
 var nome = document.getElementById("input3");
 var remove = document.getElementById("input4");
+var procura = document.getElementById("input5");
 var inputMin = document.getElementById("rangeMin");
 var inputMax = document.getElementById("rangeMax");
 var m1 = document.getElementById("m1");
 var m2 = document.getElementById("m2");
 var m3 = document.getElementById("m3");
+
+var alerta = true;
 
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
@@ -202,7 +239,7 @@ function add() {
     var campoNome = nome.value;
     if (campoId < min || campoId > max) {
         console.log("chave fora do universo possivel de chaves");
-        alert("chave fora do universo possivel de chaves");
+        if (alerta) alert("chave fora do universo possivel de chaves");
     } else {
         var item = new Item(campoId, campoNome);
         hash.add(item);
@@ -211,13 +248,27 @@ function add() {
 }
 
 function retirar() {
-
     var vlr = remove.value;
     if (vlr < min || vlr > max) {
         console.log("chave fora do universo possivel de chaves");
-        alert("chave fora do universo possivel de chaves");
+        if (alerta) alert("chave fora do universo possivel de chaves");
     } else {
         hash.excluir(vlr);
         renderHash();
     }
+}
+
+function buscar() {
+    var vlr = procura.value;
+    if (vlr < min || vlr > max) {
+        console.log("chave fora do universo possivel de chaves");
+        if (alerta) alert("chave fora do universo possivel de chaves");
+    } else {
+        hash.buscar(vlr);
+        renderHash();
+    }
+}
+
+function mudaAlerta() {
+    alerta = !alerta;
 }
